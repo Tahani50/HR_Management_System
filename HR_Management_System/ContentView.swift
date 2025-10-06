f246aaba21fb4ed6a3a0c07c46ce9c633057a1ae
Tahani50 <tahanyayman4@gmail.com>
2025-10-06 12:14:13 +0300

    Initial Commit

---
+++ HR_Management_System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HR_Management_System
+//
+//  Created by Tahani on 14/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HR_Management_System/HR_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HR_Management_SystemApp.swift
+//  HR_Management_System
+//
+//  Created by Tahani on 14/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct HR_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
